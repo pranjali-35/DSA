6e7e23e170d925abee838735978c99070bb59b6a
pranjali-35 <91174464+pranjali-35@users.noreply.github.com>
2021-12-12 19:53:01 +0530

    Update infix to postfix.c

--- infix to postfix.c
+++ infix to postfix.c
@@ -6,23 +6,22 @@
 #define SIZE 100
 
 char stack[SIZE];
-int top = -1;
 
-void push(char item){
-    if(top >= SIZE -1){
+void push(char item, int **top){
+    if(**top >= SIZE -1){
         printf("Stack overflow\n");
         return;
-    }top = top+1;
-    stack[top] = item;
+    }**top = **top+1;
+    stack[**top] = item;
 }
 
-char pop(){
+char pop(int **top){
     char item;
-    if(top < 0){
+    if(**top < 0){
         printf("Stack is empty\n");
-        return;
-    }item = stack[top];
-    top = top-1;
+        exit(0);
+    }item = stack[**top];
+    **top = **top-1;
     return item;
 }
 
@@ -42,32 +41,32 @@ int precedence(char item){
     return (0);
 }
 
-void InfixToPostfix(char infix[], char postfix[]){
+void InfixToPostfix(char infix[], char postfix[],int *top){
     int i = 0, j = 0;
     char item, x;
-    push('(');
+    push('(',&top);
     strcat(infix,")");
     item = infix[i];
     while(item != '\0'){
         if(item == '(')
-            push(item);
+            push(item,&top);
         else if(isdigit(item) || isalpha(item)){
             postfix[j] = item;
             j++;
         }else if(isOperator(item) == 1){
-            x = pop();
+            x = pop(&top);
             while(isOperator(x) == 1 && precedence(x) >= precedence(item)){
                 postfix[j] = x;
                 j++;
-                x = pop();
-            }push(x);
-            push(item);
+                x = pop(&top);
+            }push(x,&top);
+            push(item,&top);
         }else if(item == ')'){
-            x = pop();
+            x = pop(&top);
             while(x != '('){
                 postfix[j] = x;
                 j++;
-                x = pop();
+                x = pop(&top);
             }
         }else{
             printf("\nInvalid Infix expression\n");
@@ -79,10 +78,11 @@ void InfixToPostfix(char infix[], char postfix[]){
 
 int main()
 {
+    int top = -1;
     char infix[SIZE],postfix[SIZE];
-    printf("Enter infix expression : ");
+    printf("\nEnter infix expression : ");
     gets(infix);
-    InfixToPostfix(infix,postfix);
+    InfixToPostfix(infix,postfix,&top);
     printf("Postfix expression : ");
     puts(postfix);
 
